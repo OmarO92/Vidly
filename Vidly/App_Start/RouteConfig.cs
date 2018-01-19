8eec276943390053367e7cc6252b1213373a50f5
omar oseguera <omaroseguera92@gmail.com>
2018-01-18 20:25:32 -0800

    UI: Changed bootstrap theme Models: Created Movie model Controller: Created MoviesController w/ Random Action Views: Created Random View

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -9,6 +9,13 @@ namespace Vidly
 {
     public class RouteConfig
     {
+        /// <summary>
+        /// Default route
+        /// Control can be the main controller (e.g. MovieReviews)
+        /// Action is the method on the controller (e.g. TopRated())
+        /// Id another parameter currently set to optional as it is not always needed.
+        /// </summary>
+        /// <param name="routes"></param>
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
